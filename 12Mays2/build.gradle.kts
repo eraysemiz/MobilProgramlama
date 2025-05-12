fef47cec1e765c96fc41b235dfeac3cdf26dad23
eraysemiz <eraysemiz00@gmail.com>
2025-05-12 11:50:49 +0300

    12 Mayıs İkinci Ödev

---
+++ 12Mays2/build.gradle.kts
@@ -0,0 +1,15 @@
+
+plugins {
+    alias(libs.plugins.android.application) apply false
+}
+
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.1.2")
+        classpath("com.google.gms:google-services:4.4.2")
+    }
+}
